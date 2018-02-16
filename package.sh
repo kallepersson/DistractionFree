b558e09ec955318cda81c594075be465a4a63f7e
Kalle Persson <kalle@spotify.com>
2018-02-17 00:24:31 +0100

    Update package script to compress without adding directories

--- package.sh
+++ package.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-zip -r ./build/chrome-release.zip ./Chrome/*.js ./Chrome/*.json ./Chrome/*.html ./Chrome/img
-zip -r ./build/firefox-release.zip ./Firefox/*.js ./Firefox/*.json ./Firefox/*.html ./Firefox/img
+cd ./Chrome
+zip -r -FS ../build/chrome-release.zip ./*.js ./*.json ./*.html ./img
+
+cd ..
+
+cd ./Firefox
+zip -r -FS ../build/firefox-release.zip ./*.js ./*.json ./*.html ./img
